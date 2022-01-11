b2d1a6eac36e62245da480a1669d2626b7260afd
zerontec <zerontecnologia1@gmail.com>
2022-01-10 23:38:38 -0430

    componentes intro mas intro css

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,37 @@
+import './intro.css';
+
+
+const Intro = () => {
+
+return (
+  <div className="i">
+    <div className="i-left">Left </div>
+    <div className="i-left-wrapper">
+      <h2 className="i-intro">My Nombre es </h2>
+      <h1 className="i-name">Leobert Zeron </h1>
+      <div className="i-title">
+        <div className="i-title-wrapper">
+          <div className="i-title-item">Web Developer</div>
+
+          <div className="i-title-item">UI/UX Designer</div>
+
+          <div className="i-title-item">Escritor</div>
+
+          <div className="i-title-item">Web Developer</div>
+        </div>
+      </div>
+
+      <div className="i-desc">
+          Soy Desarrollador Full Stack 
+      </div>
+    </div>
+
+    <div className="i-rigth">Rigth</div>
+  </div>
+);
+
+}
+
+
+
+export default Intro 
